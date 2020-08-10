c55832f2b8369b72648765f49ee4bd4c4d48717d
herbert <2014110244@gmail.com>
2020-08-10 10:56:07 -0500

    cuarto commit

--- dbtiendavirtual.sql
+++ dbtiendavirtual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-08-2020 a las 06:22:36
+-- Tiempo de generación: 10-08-2020 a las 17:54:32
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
@@ -24,15 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `articulo`
+--
+
+CREATE TABLE `articulo` (
+  `idarticulo` int(20) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `status` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `categoria`
 --
 
 CREATE TABLE `categoria` (
-  `idCategoria` bigint(20) NOT NULL,
+  `idcategoria` int(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `descripcion` text NOT NULL,
-  `datecreated` datetime NOT NULL,
-  `status` int(11) NOT NULL
+  `status` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -53,43 +63,25 @@ CREATE TABLE `cliente` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `delivery`
+-- Estructura de tabla para la tabla `marca`
 --
 
-CREATE TABLE `delivery` (
-  `iddelivery` int(10) NOT NULL,
-  `vendedorid` int(10) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
-  `capacidad` int(10) NOT NULL
+CREATE TABLE `marca` (
+  `idmarca` int(20) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `status` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `detalle_pedido`
+-- Estructura de tabla para la tabla `modelo`
 --
 
-CREATE TABLE `detalle_pedido` (
-  `id` bigint(20) NOT NULL,
-  `pedidoid` bigint(20) NOT NULL,
-  `productoid` bigint(20) NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_temp`
---
-
-CREATE TABLE `detalle_temp` (
-  `id` bigint(20) NOT NULL,
-  `pedidoid` bigint(20) NOT NULL,
-  `productoid` bigint(20) NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `cantidad` int(11) NOT NULL
+CREATE TABLE `modelo` (
+  `idmodelo` int(20) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `status` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -108,21 +100,6 @@ CREATE TABLE `modulo` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pedido`
---
-
-CREATE TABLE `pedido` (
-  `idpedido` bigint(20) NOT NULL,
-  `personaid` bigint(20) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `monto` decimal(10,2) NOT NULL,
-  `tipopagoid` bigint(20) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `permisos`
 --
 
@@ -135,41 +112,23 @@ CREATE TABLE `permisos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `persona`
---
-
-CREATE TABLE `persona` (
-  `idpersona` bigint(20) NOT NULL,
-  `dni` varchar(50) NOT NULL,
-  `nombres` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `telefono` bigint(20) NOT NULL,
-  `email_user` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `direccion` varchar(100) NOT NULL,
-  `token` varchar(100) NOT NULL,
-  `rolid` bigint(20) NOT NULL,
-  `datecreated` datetime NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `producto`
 --
 
 CREATE TABLE `producto` (
-  `idproducto` bigint(20) NOT NULL,
-  `categoriaid` bigint(20) NOT NULL,
-  `codigo` varchar(30) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `descripcion` text NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `imagen` varchar(60) NOT NULL,
-  `datecreated` datetime NOT NULL,
-  `status` int(11) NOT NULL
+  `idproducto` int(20) NOT NULL,
+  `codigo` int(20) NOT NULL,
+  `idcategoria` int(20) NOT NULL,
+  `idarticulo` int(20) NOT NULL,
+  `idmarca` int(20) NOT NULL,
+  `idmodelo` int(20) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `talla` varchar(20) NOT NULL,
+  `genero` varchar(20) NOT NULL,
+  `botapie` varchar(30) NOT NULL,
+  `precio` varchar(20) NOT NULL,
+  `imagen` varchar(300) NOT NULL,
+  `stock` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -190,18 +149,48 @@ CREATE TABLE `rol` (
 --
 
 INSERT INTO `rol` (`idrol`, `nombrerol`, `descripcion`, `status`) VALUES
-(1, 'Encargados', 'Encargados', 1),
-(2, 'Supervisor', 'Supervisor', 2);
+(1, 'Administrador', 'Admin', 1),
+(2, 'Master', 'master', 2),
+(3, 'Cajero', 'Control Caja', 2),
+(4, 'Encargado', 'Encargado', 2),
+(5, 'Vendedor', 'Vendedor', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` bigint(20) NOT NULL,
+  `dni` varchar(50) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `email_user` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `rolid` bigint(20) NOT NULL,
+  `datecreated` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`idarticulo`);
+
+--
 -- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`idCategoria`);
+  ADD PRIMARY KEY (`idcategoria`);
 
 --
 -- Indices de la tabla `cliente`
@@ -210,16 +199,16 @@ ALTER TABLE `cliente`
   ADD PRIMARY KEY (`idcliente`);
 
 --
--- Indices de la tabla `detalle_pedido`
+-- Indices de la tabla `marca`
 --
-ALTER TABLE `detalle_pedido`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`idmarca`);
 
 --
--- Indices de la tabla `detalle_temp`
+-- Indices de la tabla `modelo`
 --
-ALTER TABLE `detalle_temp`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `modelo`
+  ADD PRIMARY KEY (`idmodelo`);
 
 --
 -- Indices de la tabla `modulo`
@@ -228,22 +217,14 @@ ALTER TABLE `modulo`
   ADD PRIMARY KEY (`idmodulo`);
 
 --
--- Indices de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`idpedido`);
-
---
--- Indices de la tabla `persona`
---
-ALTER TABLE `persona`
-  ADD PRIMARY KEY (`idpersona`);
-
---
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD PRIMARY KEY (`idproducto`);
+  ADD PRIMARY KEY (`idproducto`),
+  ADD KEY `idcategoria` (`idcategoria`),
+  ADD KEY `idarticulo` (`idarticulo`),
+  ADD KEY `idmarca` (`idmarca`),
+  ADD KEY `idmodelo` (`idmodelo`);
 
 --
 -- Indices de la tabla `rol`
@@ -252,14 +233,27 @@ ALTER TABLE `rol`
   ADD PRIMARY KEY (`idrol`);
 
 --
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD KEY `persona_rolid_foreign` (`rolid`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `idarticulo` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idCategoria` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcategoria` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
@@ -268,16 +262,16 @@ ALTER TABLE `cliente`
   MODIFY `idcliente` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_pedido`
+-- AUTO_INCREMENT de la tabla `marca`
 --
-ALTER TABLE `detalle_pedido`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `marca`
+  MODIFY `idmarca` int(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_temp`
+-- AUTO_INCREMENT de la tabla `modelo`
 --
-ALTER TABLE `detalle_temp`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `modelo`
+  MODIFY `idmodelo` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `modulo`
@@ -286,28 +280,41 @@ ALTER TABLE `modulo`
   MODIFY `idmodulo` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `idpedido` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `persona`
---
-ALTER TABLE `persona`
-  MODIFY `idpersona` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idproducto` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idproducto` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_3` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_4` FOREIGN KEY (`idmodelo`) REFERENCES `modelo` (`idmodelo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `persona_rolid_foreign` FOREIGN KEY (`rolid`) REFERENCES `rol` (`idrol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
